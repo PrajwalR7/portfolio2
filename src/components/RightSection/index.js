35992dd771d2a3c975c6d69e097b7be16bd7d39c
Prajwal R <rameshprajwal21@gmail.com>
2022-10-04 02:07:25 +0530

    scroll

--- src/components/RightSection/index.js
+++ src/components/RightSection/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './style.css';
 import profile from '../../resources/profile1.jpg';
-// import { useMediaQuery } from '@chakra-ui/react';
 // import 'boxicons';
-// import { Link } from 'react-scroll';
+import { Link } from 'react-scroll';
 export default function RightSection() {
-  // let [bigScreen] = useMediaQuery('(min-width:1000px)');
   return (
     <div id='right-sec'>
       <div id='right-left-sec'>
@@ -17,9 +15,9 @@ export default function RightSection() {
           Im a creative designer based in India and im very passionate and
           dedicated to my work
         </p>
-        {/* <Link to='contact-link' spy={true} smooth={true} offset={-80}> */}
-        <button id='say-hello'>Say Hello</button>
-        {/* </Link> */}
+        <Link to='contact-link' spy={true} smooth={true} offset={-80}>
+          <button id='say-hello'>Say Hello</button>
+        </Link>
       </div>
       <div id='right-right-sec'>
         <img src={profile} alt='profile' />
